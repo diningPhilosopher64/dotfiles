f90873cdeaca3c2ae2da62a562b3157ccac0f1c1
skondapa <skondapa@mathworks.com>
2022-07-29 11:44:05 +0530

    Added lualine config

--- lvim/init.lua
+++ lvim/init.lua
@@ -2,6 +2,12 @@
 local current_dir = string.gsub(debug.getinfo(1).source, "^@(.+/)[^/]+$", "%1")
 
 dofile(current_dir .. "keymaps.lua")
-dofile(current_dir .. "plugins.lua")
 dofile(current_dir .. "options.lua")
+
+
+-- Plugins with default config
+dofile(current_dir .. "plugins.lua")
+
+-- Plugins with custom config
 dofile(current_dir .. "trouble.lua")
+dofile(current_dir .. "lualine.lua")
