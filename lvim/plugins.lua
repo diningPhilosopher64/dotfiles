794ec17e70ded943bbfd77a50155189ff65cbced
skondapa <skondapa@mathworks.com>
2022-07-29 11:46:16 +0530

    Formatting

--- lvim/plugins.lua
+++ lvim/plugins.lua
@@ -1,9 +1,9 @@
 lvim.plugins = {
-  { "folke/tokyonight.nvim" },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  { "fatih/vim-go", run = ':GoUpdateBinaries' },
-  { "p00f/nvim-ts-rainbow" },
+    { "folke/tokyonight.nvim" },
+    {
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+    },
+    { "fatih/vim-go", run = ':GoUpdateBinaries' },
+    { "p00f/nvim-ts-rainbow" },
 }
