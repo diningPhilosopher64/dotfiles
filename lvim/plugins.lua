09f1957f1449a70c0387a0aea21424eb501aa274
Sourabh Kondapaka <skondapa@mathworks.com>
2022-10-12 11:10:26 +0530

    Minor change to .bashrc.sourabh. Updated plugins.lua to fix error with trouble.nvim not loading

--- lvim/plugins.lua
+++ lvim/plugins.lua
@@ -2,7 +2,14 @@ lvim.plugins = {
     { "folke/tokyonight.nvim" },
     {
         "folke/trouble.nvim",
-        cmd = "TroubleToggle",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
     },
     { "fatih/vim-go", run = ':GoUpdateBinaries' },
     { "p00f/nvim-ts-rainbow" },
